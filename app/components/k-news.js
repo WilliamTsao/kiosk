e2b0c3f036dba9209aed630797c08b04c1778287
Nick Sahler <nick@sahler.co>
2016-03-02 19:01:28 -0500

    Create news feed component

---
+++ app/components/k-news.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  page: Ember.computed('article', function() {
+    return this.get('articles').splice(0,5)
+  })
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  model() {
+    // From https://ajax.googleapis.com/ajax/services/feed/load?v=2.0&q=http://blog.nj.com/middlesex_impact/rss.xml&num=20
+    return $.get( '/news.json' ).then(
+      function(data) {
+        return {
+          news: data.responseData.feed.entries
+        }
+      }
+    );
+  }
 });
