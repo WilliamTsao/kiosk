d9e9f462c93dec62ecd0984a4557bea29d78aad1
Nick Sahler <nick@sahler.co>
2016-09-02 08:21:18 -0400

    Add editing and reorganize entity schema

--- app/controllers/send.js
+++ app/controllers/send.js
@@ -9,12 +9,21 @@ export default Ember.Controller.extend({
 
   loading: null,
 
+  is_sent: Ember.computed.equal('loading', 'sent'),
+  is_loading: Ember.computed.equal('loading', 'loading'),
+
+  not_editing: function() {
+    return !this.get('editing');
+  }.property('editing'),
+
   actions: {
     set_medium(medium) {
       this.set('medium', medium);
     },
 
     send() {
+      var self = this;
+
       this.set('loading', 'loading');
       var to;
       var medium = this.get('medium');
@@ -31,8 +40,39 @@ export default Ember.Controller.extend({
         break;
       }
 
-      $.get(`http://api.innovatenb.org/send/${medium}/${this.get('model.id')}?to=${to}`).then(function() {
-        this.set('loading', 'sent');
+      Ember.$.get(`http://api.innovatenb.org/send/${medium}/${this.get('model.id')}?to=${to}`)
+        .then(function() {
+          self.set('loading', 'sent');
+        });
+    },
+
+    reset() {
+      this.set('loading', null);
+    },
+
+    edit() {
+      this.set('editing', true);
+    },
+
+    save() {
+      this.set('editing', false);
+      var x = this.get('model');
+      $.ajax({
+        url: `http://api.innovatenb.org/entities/${this.get('model.id')}?token=bacon`,
+        type: "POST",
+        contentType : 'application/json',
+        data: JSON.stringify({
+          id: x.id,
+          name: x.name,
+          description: x.description,
+          tags: x.tags,
+          address: x.address,
+          location: x.location,
+          phone: x.phone,
+          url: x.url,
+          image_url: x.image_url,
+          icon_url: x.icon_url
+        })
       });
     }
   }
